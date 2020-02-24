5330357193631d1d026261bf829e4a7c97d4b0e9
zukizukimaru <kazucchi.98323@gmail.com>
2020-02-24 16:13:19 +0900

    配列で書いて文を短く書いた。そして、末吉も追加した

--- js/main.js
+++ js/main.js
@@ -4,21 +4,24 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click' , () => {
-    const n = Math.floor(Math.random() * 3);
-    // btn.textContent = n;
+    const results = ['大吉' , '中吉' , '凶' , '末吉'];
+    // const n = Math.floor(Math.random() * results.length);
+    // btn.textContent =results[n];
+    btn.textContent =results[Math.floor(Math.random() * results.length)];
+    
 
 
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = '大吉';
+    //     break;
+    //   case 1:
+    //     btn.textContent = '中吉';
+    //     break;
+    //   case 2:
+    //     btn.textContent = '凶';
+    //     break;
 
-    }
+    // }
   });
 }
